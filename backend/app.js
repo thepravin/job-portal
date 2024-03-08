e9814a36971d21575a9cf391393a89c375b77091
Pravin Nalawade <thepravin28@gmail.com>
2024-03-08 13:30:21 +0530

    cors setup

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,12 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config()
 
+// cors setup
+const cors = require('cors');
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","DELETE","PUT"],
+    credentials:true
+}))
+
 module.exports = app;
