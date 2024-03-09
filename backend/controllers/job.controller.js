2d8f1261c469165f28b36bee29218a8615d5d732
Pravin Nalawade <thepravin28@gmail.com>
2024-03-09 18:39:39 +0530

    job routers setup

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,140 @@
+const catchAsyncError = require("../middlewares/catchAsyncError");
+const { ErrorHandler } = require("../middlewares/error");
+const Job = require("../models/job.model");
+
+const getAllJobs = catchAsyncError(async (req, res, next) => {
+  const jobs = await Job.find({ expired: false });
+  res.status(200).json({
+    success: true,
+    jobs,
+  });
+});
+
+const postJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker is not allowed to Post Job !!", 400)
+    );
+  }
+  const {
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo,
+  } = req.body;
+
+  if (!title || !description || !category || !country || !city || !location) {
+    return next(new ErrorHandler("Please provide full job details.", 400));
+  }
+
+  if ((!salaryFrom || !salaryTo) && !fixedSalary) {
+    return next(
+      new ErrorHandler(
+        "Please either provide fixed salary or ranged salary.",
+        400
+      )
+    );
+  }
+
+  if (salaryFrom && salaryTo && fixedSalary) {
+    return next(
+      new ErrorHandler("Cannot Enter Fixed and Ranged Salary together.", 400)
+    );
+  }
+  const postedBy = req.user._id;
+  const job = await Job.create({
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo,
+    postedBy,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Job Posted Successfully!",
+    job,
+  });
+});
+
+
+const myjobs = catchAsyncError(async(req,res,next)=>{
+    const{role}=req.user;
+    if (role === "Job Seeker") {
+        return next(
+          new ErrorHandler("Job Seeker is not allowed to Post Job !!", 400)
+        );
+      }
+
+      const myJobs = await Job.find({postedBy:req.user._id});
+      
+      res.status(200).json({
+        success:true,
+        myJobs,
+      })
+
+})
+
+const updateJob = catchAsyncError(async(req,res,next)=>{
+    const{role}=req.user;
+    if (role === "Job Seeker") {
+        return next(
+          new ErrorHandler("Job Seeker is not allowed to Post Job !!", 400)
+        );
+      }
+
+    const {id}=req.params;
+    let job = await Job.findById(id);
+    if(!job){
+        return next(
+            new ErrorHandler("Oops, Job not found!!",404)
+        )
+    }
+
+    job=await Job.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:true,
+        job,
+        message:"Job Updated Successfully"
+    })
+})
+
+
+const deleteJob = catchAsyncError(async(req,res,next)=>{
+    const{role}=req.user;
+    if (role === "Job Seeker") {
+        return next(
+          new ErrorHandler("Job Seeker is not allowed to Post Job !!", 400)
+        );
+      }
+
+      const {id}=req.params;
+      let job = await Job.findById(id);
+      if(!job){
+        return next(new ErrorHandler("Oops, Job not found !",404))
+      }
+
+      await job.deleteOne();
+      res.status(200).json({
+        success:true,
+        message:"Job Deleted Successfully !!"
+      })
+})
+
+
+module.exports = { getAllJobs, postJob,myjobs ,updateJob,deleteJob};
--- backend/router/jobRouter.js
+++ backend/router/jobRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { getAllJobs, postJob, myjobs, updateJob, deleteJob } = require('../controllers/job.controller');
+const isAuthorized = require('../middlewares/auth');
 const router = express.Router();
 
 
-
+router.get("/getall",getAllJobs)
+router.post("/post",isAuthorized,postJob)
+router.get("/getmyjobs",isAuthorized,myjobs)
+router.put("/update/:id",isAuthorized,updateJob)
+router.delete("/delete/:id",isAuthorized,deleteJob)
 
 
 
