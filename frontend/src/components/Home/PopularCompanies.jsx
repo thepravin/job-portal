b5658e83265c2d44cef8fbabee9474897ed2cad6
Pravin Nalawade <thepravin28@gmail.com>
2024-03-13 15:07:31 +0530

    home page complete

--- frontend/src/components/Home/PopularCompanies.jsx
+++ frontend/src/components/Home/PopularCompanies.jsx
@@ -1,9 +1,54 @@
 
+import { FaMicrosoft, FaApple } from "react-icons/fa";
+import { SiTesla } from "react-icons/si";
 
 const PopularCompanies = () => {
+  const companies = [
+    {
+      id: 1,
+      title: "Microsoft",
+      location: "Street 10 Hinjawadi, Pune",
+      openPositions: 10,
+      icon: <FaMicrosoft />,
+    },
+    {
+      id: 2,
+      title: "Google",
+      location: "Tower E Old Madras Road 4th and 5th Floors Bangalore,",
+      openPositions: 5,
+      icon: <SiTesla />,
+    },
+    {
+      id: 3,
+      title: "Apple",
+      location: "UB City, 19th Floor, Concorde Tower C, Vittal Mallya Rd, Bengaluru",
+      openPositions: 20,
+      icon: <FaApple />,
+    },
+  ];
   return (
-    <div>PopularCompanies</div>
-  )
-}
+    <div className="companies">
+      <div className="container">
+        <h3>TOP COMPANIES</h3>
+        <div className="banner">
+          {companies.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="content">
+                  <div className="icon">{element.icon}</div>
+                  <div className="text">
+                    <p>{element.title}</p>
+                    <p>{element.location}</p>
+                  </div>
+                </div>
+                <button>Open Positions {element.openPositions}</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PopularCompanies
